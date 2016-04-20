737baba2481c26e2239ffc5d3677cc1129b7d1e2
Jelani Thompson <jelanithompson@hotmail.com>
2016-04-20 10:46:43 -0400

    In progress

--- RomanNumeral.js
+++ RomanNumeral.js
@@ -2,7 +2,7 @@ function convertToRoman(num) {
     var numbers = [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000];
     var numeralList = ['I', 'IV', 'V', 'IX', 'X', 'XL', 'L', 'XC', 'C', 'CD', 'D', 'CM', 'M'];
 
-    //If provided number argument doesn't match anything in the numbers array, add up to get to it
+    //If provided number argument doesn't match anything in the numbers array, add to get to it
     if (numbers.indexOf(num) == -1) {
         console.log("Houston, we have a problem");
     }
